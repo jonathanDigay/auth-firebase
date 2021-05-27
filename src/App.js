b80bfe78554487b20f7d0862641c310d7f2654b7
Loading <jonathan.digay1@mail.com>
2021-05-27 21:57:44 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
 import {AuthProvider} from "./Context/context"
 import {Privateroutes} from "./Private/Privateroutes"
 // components
-import Home from "./Components/Home"
+import Home from "./Components/home.js"
 import SignIn from "./Components/Signin"
 import SignUp from "./Components/Signup"
 import Profile from "./Components/Profile"

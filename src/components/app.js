6767eb054c7e42bdc9d404ced93f78ad8e31af6e
Loading <jonathan.digay1@mail.com>
2021-05-28 23:51:44 +0800

    initial

--- src/components/app.js
+++ src/components/app.js
@@ -4,19 +4,19 @@ import { AuthProvider } from "../userscontext/context"
 import { Privateroutes } from "../private/privateroutes"
 import "./app.scss"
 // components
-import SignIn from "./signin"
-import SignUp from "./signup"
-import Profile from "./profile"
-import Home from "./home"
+import signin from "./signin"
+import signup from "./signup"
+import profile from "./profile"
+import home from "./home"
 const App = () => {
     return (
         <>
                 <Switch>
                     <AuthProvider>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/signin" component={SignIn} />
-                        <Route path="/signup" component={SignUp} />
-                        <Privateroutes path="/profile" component={Profile} />
+                        <Route exact path="/" component={home} />
+                        <Route path="/signin" component={signin} />
+                        <Route path="/signup" component={signup} />
+                        <Privateroutes path="/profile" component={profile} />
                     </AuthProvider>
                 </Switch>
         </>
